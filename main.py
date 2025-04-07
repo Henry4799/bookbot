f7bcdb52b86fea603b72e7bbc4b75c99f52588bc
Henry4799 <henry.enholm@gmail.com>
2025-04-07 12:13:45 -0400

    Refactoring code

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+from stats import word_count
+
+file_path = ("")
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    file_path = "books/frankenstein.txt"
+    book_text = get_book_text(file_path)
+    count = word_count(book_text)
+    print(f"{count} words found in the document")
+
+main()
---
+++ stats.py
@@ -0,0 +1,10 @@
+
+def word_count(book_text):
+    text = book_text
+    words = text.split()
+    count = len(words)
+    return(count)
+
+# def character_count(book_text):
+    characters = {}
+    text = book_text.lower()
